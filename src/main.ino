c1813b6b76a94d0fdf0d5e19970b7704eb473cec
Gary <netzver@gmail.com>
2017-03-25 16:20:37 +0200

    add psycho buzzer

--- src/main.ino
+++ src/main.ino
@@ -21,18 +21,71 @@
 #include <ESP8266HTTPClient.h>
 #include <FirebaseArduino.h>
 
+#include "melodies.h"
+extern "C" {
+  #include "user_interface.h"
+}
+
 #define FIREBASE_HOST "iottestcheb.firebaseio.com"
 
 #define FIREBASE_AUTH "D9b1YvaQh3YFo4AAbT6BNAcRir7PUmWFIakZBr3v"
 #define WIFI_SSID "ekreative"
 #define WIFI_PASSWORD "yabloka346"
 
+// http://www.14core.com/wp-content/uploads/2015/06/Node-MCU-Pin-Out-Diagram1.png
 const int MOTOR_L1 = 5; // D1
 const int MOTOR_L2 = 4; // D2
 const int MOTOR_R1 = 0; // D3
 const int MOTOR_R2 = 2; // D4
+const int BUZZER = 14; // D5
+
 const String ROBOT_ID = "";
 const bool DEBUG = true;
+
+int currentMelody = 0;
+
+
+os_timer_t myTimer;
+
+int buzz_note = 1;
+int buzz_melody_num = 0;
+bool buzz_playing = false;
+bool buzz_delay = false;
+unsigned long buzz_next_fire = 0;
+
+void timerCallback(void *pArg) {
+  if (!buzz_playing) {return;}
+
+  unsigned long currentMillis = millis();
+  if (buzz_next_fire < currentMillis) {
+    if (buzz_note > buzz_melodies[buzz_melody_num * 2][0]) {
+      buzz_playing = false;
+      Serial.println("finish");
+      return;
+    }
+
+    if (!buzz_delay) { //play note
+      int frequency = buzz_melodies[buzz_melody_num * 2][buzz_note];
+      if (frequency) {
+        tone(BUZZER, frequency);
+      }
+      // note duration
+      buzz_next_fire = currentMillis + 1000/buzz_melodies[buzz_melody_num * 2 + 1][buzz_note];
+      buzz_delay = true;
+    } else {
+      noTone(BUZZER);
+      buzz_next_fire = currentMillis + 1000/buzz_melodies[buzz_melody_num * 2 + 1][buzz_note] * 1.2;
+      buzz_delay = false;
+      buzz_note++;
+    }
+  }
+}
+
+void timerInit(void) {
+  // http://www.switchdoc.com/2015/10/iot-esp8266-timer-tutorial-arduino-ide/
+  os_timer_setfn(&myTimer, timerCallback, NULL);
+  os_timer_arm(&myTimer, 10, true);
+}
 
 void setup() {
 
@@ -43,6 +96,8 @@ void setup() {
   pinMode(MOTOR_L2, OUTPUT);
   pinMode(MOTOR_R1, OUTPUT);
   pinMode(MOTOR_R2, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
 
   // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -56,12 +111,9 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  // Firebase.set("pushbutton", 0);
-  // Firebase.set("sunlight", 0);
-  // Firebase.set("redlight", 0);
-  // Firebase.set("cooldown", 0);
-  // Firebase.set("brrr", 0);
 
+  play(0);
+  timerInit();
 }
 
 
@@ -70,26 +122,33 @@ int motor_l = 0;
 int motor_r = 0;
 
 void loop() {
-  Serial.println("loop ");
+  Serial.print(".");
 
   motor_l = Firebase.getInt(ROBOT_ID + "motor_l");
   motor_r = Firebase.getInt(ROBOT_ID + "motor_r");
 
   setMotorState(motor_l, motor_r);
 
- // int newButton = digitalRead(buttonPin);
- //  if (newButton != button) {
- //    button = newButton;
- //    Firebase.setInt("pushbutton", button);
- //  }
- //  float newLight = analogRead(lightSensorPin);
- //  if (abs(newLight - light) > 100) {
- //    light = newLight;
- //    Firebase.setFloat("sunlight", light);
- //  }
+  play(0);
 
-  delay(200);
+  delay(50);
+}
 
+bool play(int melody_num) {
+  if (buzz_playing) {
+    return false;
+  }
+
+  // buzz_melody_num = melody_num;
+  buzz_melody_num = currentMelody++;
+  if (currentMelody > 30) {
+    currentMelody = 0;
+  }
+
+  buzz_note = 1;
+  buzz_playing = true;
+  buzz_next_fire = 0;
+  return true;
 }
 
 void setMotorState(int motor_l, int motor_r) {
